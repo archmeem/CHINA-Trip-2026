5952504b85eaded4b8b82cfdfa18568897a369f7
archmeem <arch.me.em@gmail.com>
2026-09-01 12:28:51 -0400

    Refresh app shell cache after checklist wiring

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ct26-checklist3',A=['./','./index.html','./styles.css','./data.js','./app.js','./checklist.js','./manifest.webmanifest','./icon.svg'];
+const C='ct26-checklist4',A=['./','./index.html','./styles.css','./data.js','./app.js','./checklist.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
