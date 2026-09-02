63527fc4c7e82b3e481b4274aefb74c0e9e77718
archmeem <arch.me.em@gmail.com>
2026-09-01 23:54:36 -0400

    Cache QC fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ct26-china-offline6';
+const CACHE='ct26-china-offline7';
 const SHELL=['./','./index.html','./styles.css','./today.css','./data.js','./plan-update.js','./plan-fix.js','./app.js','./stability-hotfix.js','./today.js','./trip-fix.js','./checklist.js','./sync.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
